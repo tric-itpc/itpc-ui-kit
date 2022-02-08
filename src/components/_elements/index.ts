9c4b50320435f1d51d14f0cf8cb2ad58d68470d9
Artem Noskov <artyom.noskov@gmail.com>
2022-02-08 17:42:44 +0500

    TextArea AutoSize

--- src/components/_elements/index.ts
+++ src/components/_elements/index.ts
@@ -1,5 +1,5 @@
 export { Popover } from './Popover'
-export { Input, InputWrap } from './Input'
+export { Input, InputProps, InputWrap, InputWrapProps } from './Input'
 export { Placeholder } from './Placeholder'
 export { SelectButton, SelectItem, SelectItemFlag, SelectWrap } from './Select'
 
