599a7657788fe651c13b006f85cbfe4d360c1de6
Artem Noskov <artyom.noskov.1998@gmail.com>
2023-06-09 21:34:53 +0500

    v.3.0.1 Исправление стилей плейсхолдера.

---
+++ dist/components/_elements/InputIcon/index.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+interface InputWrapProps {
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    children: React.ReactNode;
+}
+export declare const InputIcon: React.FC<InputWrapProps>;
+export {};
