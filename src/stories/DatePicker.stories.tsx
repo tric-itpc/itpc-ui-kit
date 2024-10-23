ddf18dc70b29e648f63c99dcf9afb1d668b572bb
Сорокин Денис Николаевич <sdntmn@itpc.ru>
2024-10-23 12:57:10 +0500

    Добавлен расчет положения календаря при absolute
    Отредактирована документация к календарю

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -4,13 +4,14 @@ import { ComponentMeta, ComponentStory } from "@storybook/react"
 
 import { FormattedValues, Text } from "../components"
 import { DatePicker, Props } from "../components/DatePicker"
+import type { PositionType } from "../components/DatePicker/types"
 
 export default {
   component: DatePicker,
   title: "Components/DatePicker",
-} as ComponentMeta<React.FC<Props>>
+} as ComponentMeta<React.FC<Props<PositionType>>>
 
-const Template: ComponentStory<React.FC<Props>> = (args) => {
+const Template: ComponentStory<React.FC<Props<PositionType>>> = (args) => {
   const [date, setDate] = useState<FormattedValues>({
     formattedValue: "",
     value: "",
@@ -34,5 +35,6 @@ export const Basic = Template.bind({})
 Basic.args = {
   errorMessage: "Error message",
   placeholder: "Enter date",
+  position: "absolute",
   withTime: false,
 }
