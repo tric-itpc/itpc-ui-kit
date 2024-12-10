fb2bd4e06e26dbab878582e78516ecbce33cfdb2
Artem Noskov <artyom.noskov.1998@gmail.com>
2024-12-10 17:32:50 +0500

    Layout.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,11 @@
-import "../src/styles/global.css"
+import React from "react"
+
 import { Preview } from "@storybook/react"
 
 import { withThemeByClassName } from "@storybook/addon-themes"
+
+import "../src/styles/global.css"
+import { Layout } from "../src/components/layout/Layout"
 
 const preview: Preview = {
   tags: ["autodocs"],
@@ -14,6 +18,19 @@ const preview: Preview = {
       parentSelector: "body",
       defaultTheme: "light",
     }),
+    (story) => (
+      <Layout
+        style={{
+          padding: "2em",
+          width: "auto",
+          border: `1px solid var(--itpc-primary-border-color)`,
+          borderRadius: "4px",
+          minHeight: "500px",
+        }}
+      >
+        {story()}
+      </Layout>
+    ),
   ],
   parameters: {
     options: {
